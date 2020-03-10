822959c543d171651f0a43bc7f388271b578daac
gabrielguimaraes05 <gabriel.prguimaraes@gmail.com>
2020-03-10 08:06:49 -0300

    Alteração do fluxo

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 const fs = require('fs');
-const answerController = require('./controller/answerController');
 const sha1 = require('js-sha1');
+const FormData = require('form-data');
 
-const getBreeds = async () => {
+const request = async () => {
   try {
     return await axios.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=88dc014aff9c81caa0aa8aa5fba846b897bca63f')
   } catch (error) {
@@ -11,18 +11,54 @@ const getBreeds = async () => {
   }
 }
 
-const countBreeds = async () => {
-  const breeds = await getBreeds()
+const decrypt = async (cifrado, deslocamento) => {
+  var string = '';
 
-  if (breeds.data) {
-    var resposta = JSON.stringify(breeds.data);
+  for (var i = 0; i < cifrado.length; i++) {
+    if (cifrado.charCodeAt(i) >= 97 && cifrado.charCodeAt(i) <= 122) {
+      var codigo = cifrado.charCodeAt(i) - deslocamento;
+      if (codigo >= 97 && codigo <= 122) {
+        string += String.fromCharCode(codigo);
+      } else {
+        string += String.fromCharCode(((codigo + 122) - 97) + 1)
+      }
+    } else {
+      string += cifrado[i];
+    }
+  }
+  return string;
+}
 
-    fs.writeFileSync('./answer_repository/answer.json', resposta);
+const dispatch = async () => {
+  const bodyFormData = new FormData();
 
-    var obj = await answerController.index();
-    obj.resumo_criptografico = sha1(obj.decifrado)
-    console.log(obj);
+  try {
+    bodyFormData.append('answer', './answer_repository/answer.json');
+  } catch (response) {
+    console.log(response)
+  }
+
+  axios.post('https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=88dc014aff9c81caa0aa8aa5fba846b897bca63f', bodyFormData, {
+    headers: form.getHeaders(),
+  }).then(result => {
+    // Handle result…
+    console.log(result.data);
+  });
+
+}
+
+const index = async () => {
+
+  const response = await request();
+
+  if (response.data) {
+
+    response.data.decifrado = await decrypt(response.data.cifrado, response.data.numero_casas);
+    response.data.resumo_criptografico = sha1(response.data.decifrado);
+    fs.writeFileSync('./answer_repository/answer.json', JSON.stringify(response.data));
+
+    await dispatch();
   };
 }
 
-countBreeds();
+index();
